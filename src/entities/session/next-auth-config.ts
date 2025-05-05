f0c335a631c5a38b69eb81e29ef76398194329b3
cpt.kokao <cpt.kokao@gmail.com>
2025-05-05 14:51:52 +0300

    feat: add auth types

--- src/entities/session/next-auth-config.ts
+++ src/entities/session/next-auth-config.ts
@@ -1,13 +1,18 @@
 import { AuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { dbClient } from "@/shared/lib/db";
+import { privateConfig } from "@/shared/config/private";
+import { compact } from "lodash-es";
 
 export const nextAuthConfig: AuthOptions = {
-  // Configure one or more authentication providers
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID ?? "",
-      clientSecret: process.env.GITHUB_SECRET ?? "",
-    }),
-    // ...add more providers here
-  ],
+  adapter: PrismaAdapter(dbClient),
+  providers: compact([
+    privateConfig.GITHUB_ID &&
+      privateConfig.GITHUB_SECRET &&
+      GithubProvider({
+        clientId: process.env.GITHUB_ID ?? "",
+        clientSecret: process.env.GITHUB_SECRET ?? "",
+      }),
+  ]),
 };
---
+++ src/shared/config/private.ts
@@ -0,0 +1,36 @@
+import { z } from "zod";
+
+const privateConfigSchema = z.object({
+  GITHUB_ID: z.string().optional(),
+  GITHUB_SECRET: z.string().optional(),
+
+  //   ADMIN_EMAILS: z.string().optional(),
+  //   TEST_EMAIL_TOKEN: z.string().optional(),
+
+  //   EMAIL_SERVER_USER: z.string(),
+  //   EMAIL_SERVER_PASSWORD: z.string(),
+  //   EMAIL_SERVER_HOST: z.string(),
+  //   EMAIL_SERVER_PORT: z.string(),
+  //   EMAIL_FROM: z.string(),
+
+  //   S3_ACCESS_KEY_ID: z.string(),
+  //   S3_SECRET_ACCESS_KEY: z.string(),
+  //   S3_IMAGES_BUCKET: z.string(),
+  //   S3_ENDPOINT: z.string(),
+  //   S3_REGION: z.string(),
+
+  //   CONTENT_URL: z.string(),
+  //   CONTENT_TOKEN: z.string().optional(),
+
+  //   EVENT_STORE_DB_URL: z.string(),
+  //   REDIS_URL: z.string(),
+
+  //   PRODAMUS_KEY: z.string().optional(),
+  //   PRODAMUS_URL: z.string().optional(),
+  //   PRODAMUS_DEMO_ENABLED: z
+  //     .string()
+  //     .optional()
+  //     .transform<boolean>((value) => value === "true"),
+});
+
+export const privateConfig = privateConfigSchema.parse(process.env);
