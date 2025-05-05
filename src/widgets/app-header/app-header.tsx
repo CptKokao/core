f0e25008ec227d06cf6508bbf8b05782625ef61f
cpt.kokao <cpt.kokao@gmail.com>
2025-05-05 13:30:56 +0300

    fix: linter error

--- src/widgets/app-header/app-header.tsx
+++ src/widgets/app-header/app-header.tsx
@@ -1,9 +1,9 @@
-import { Button } from "@/shared/ui/button";
+// import { Button } from "@/shared/ui/button";
 import { Layout } from "./_ui/layout";
 import { Logo } from "./_ui/logo";
 import { MainNav } from "./_ui/main-nav";
 import { Profile } from "./_ui/profile";
-import { HelpCircle } from "lucide-react";
+// import { HelpCircle } from "lucide-react";
 import { ToggleTheme } from "@/features/theme/theme-toogle";
 
 export function AppHeader({
